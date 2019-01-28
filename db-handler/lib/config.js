78485fd1e416d289d2047a3deeb9e95bcdc0e18c
vardy <jarred.vardy@gmail.com>
2019-01-28 04:53:50 +0000

    Ignored and removed config.json in favour of example file.

--- db-handler/lib/config.js
+++ db-handler/lib/config.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
 
 function getConfig (path) {
+	// Redis IP
+	// Redis DB
+	// Redis User
+	// Redis Pass
+	// Redis container Name
+	// Redis Port
 	return JSON.parse(fs.readFileSync(path, 'utf8'));
 }
 
