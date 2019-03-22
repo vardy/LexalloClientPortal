d14f29de3de8545400003f5d9686c4c180268c2e
vardy <jarred.vardy@gmail.com>
2019-03-22 06:21:47 +0000

    Added hidden 'locked' field to stop nullification of file attribute

--- resources/views/files/edit.blade.php
+++ resources/views/files/edit.blade.php
@@ -1,44 +1,46 @@
-@extends('layouts.master')
+@extends('layouts.main')
 
 @section('title', 'Files/Edit')
 
-@section('page_heading', 'Files page')
+@section('sub_css_imports')
+    <link rel="stylesheet" href="{{ mix('/css/files.css') }}" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+@endsection
 
-@section('content')
+@section('sub_content')
     @include('files.file-nav-buttons')
 
-    <section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
-        <div class="mdl-card mdl-cell mdl-cell--12-col">
-            <div class="mdl-card__supporting-text mdl-grid mdl-grid--no-spacing">
-                <h4 class="mdl-cell mdl-cell--12-col">Editing: {{ $file->id }}</h4>
+    <div class="edit-card-container">
+        <div class="card">
 
-                <form id="form_edit" method="POST" action="/files/{{ $file->id }}">
-                    {{ method_field('PATCH') }}
-                    {{ csrf_field() }}
+            <form id="form_edit" method="POST" action="/files/{{ $file->id }}">
+                {{ method_field('PATCH') }}
+                {{ csrf_field() }}
 
-                    <div class="mdl-textfield mdl-js-textfield">
-                        <textarea class="mdl-textfield__input" type="text" rows= "4" id="notes" name="notes">{{ $file->notes }}</textarea>
-                        <label class="mdl-textfield__label" for="notes">Notes...</label>
-                    </div>
-                </form>
+                <div>
+                    <input type="hidden" name="locked" value="{{ $file->locked }}">
+                </div>
 
-                @if($errors->any())
-                    <div>
-                        <ul>
-                            @foreach($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
-            </div>
+                <div>
+                    <label for="notes">Notes...</label>
+                    <textarea type="text" rows= "4" id="notes" name="notes">{{ $file->notes }}</textarea>
+                </div>
+            </form>
 
-            <div class="mdl-card__actions">
-                <a href="#" class="mdl-button" onclick="document.getElementById('form_edit').submit();">
-                    Submit
-                </a>
-            </div>
+            @if($errors->any())
+                <div>
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            <a href="#" class="submit-button" onclick="document.getElementById('form_edit').submit();">
+                Submit
+            </a>
 
         </div>
-    </section>
+    </div>
 @endsection
