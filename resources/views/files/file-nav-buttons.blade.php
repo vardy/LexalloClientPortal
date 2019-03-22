b465e6a7bb1bd214cc00b4adbf7bc2866a3c053c
vardy <jarred.vardy@gmail.com>
2019-03-22 06:11:43 +0000

    Swapped order of nav buttons to be more logical

--- resources/views/files/file-nav-buttons.blade.php
+++ resources/views/files/file-nav-buttons.blade.php
@@ -1,14 +1,14 @@
 <div class="files-nav-buttons">
-    <span id="upload-button">
-        <a href="/files/upload">
-            <i class="fas fa-folder-plus files-nav-icon"></i><br>
-            Upload a file
-        </a>
-    </span>
     <span id="list-button">
         <a href="/files">
             <i class="fas fa-list-ul files-nav-icon"></i><br>
             List files
+        </a>
+    </span>
+    <span id="upload-button">
+        <a href="/files/upload">
+            <i class="fas fa-folder-plus files-nav-icon"></i><br>
+            Upload a file
         </a>
     </span>
 </div>
