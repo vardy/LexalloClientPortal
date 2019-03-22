58490087610af07515d4d222702863d05a766266
vardy <jarred.vardy@gmail.com>
2019-03-22 03:04:12 +0000

    Added CSS files for uploads index page

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,7 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/home_page.scss', 'public/css')
     .sass('resources/sass/landing.scss', 'public/css')
     .sass('resources/sass/main.scss', 'public/css')
+    .sass('resources/sass/files.scss', 'public/css')
     .extract();
 
 if (mix.inProduction()) {
