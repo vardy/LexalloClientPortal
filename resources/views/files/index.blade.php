26ff36bdcfe31546b628edd8cae9d9d1a4f92b42
vardy <jarred.vardy@gmail.com>
2019-03-22 05:32:56 +0000

    Styling for files index page

--- resources/views/files/index.blade.php
+++ resources/views/files/index.blade.php
@@ -2,8 +2,14 @@
 
 @section('title', 'Files')
 
+@section('sub_css_imports')
+    <script src="{{ mix('/js/sorttable.js') }}"></script>
+    <link rel="stylesheet" href="{{ mix('/css/files.css') }}" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+@endsection
+
 @section('sub_content')
-    <div>
+    <!-- <div>
         <a href="/files/upload">Upload a file.</a>
 
         <ul>
@@ -28,5 +34,62 @@
                 </li>
             @endforeach
         </ul>
-    </div>
+    </div> -->
+        <div class="files-nav-buttons">
+            <span id="upload-button">
+                <a href="/files/upload">
+                    <i class="fas fa-folder-plus files-nav-icon"></i><br>
+                    Upload a file
+                </a>
+            </span>
+            <span id="list-button">
+                <a href="/files">
+                    <i class="fas fa-list-ul files-nav-icon"></i><br>
+                    List files
+                </a>
+            </span>
+        </div>
+
+        <div class="file-table-container">
+            <div class="card">
+                <table class="file-table sortable">
+                    <tr class="heading-row">
+                        <th class="heading-name">
+                            File
+                        </th>
+                        <th class="heading-size">
+                            Size
+                        </th>
+                        <th class="heading-type td-right">
+                            Type
+                        </th>
+                    </tr>
+
+                    @foreach($files as $file)
+                        <tr class="normal-row">
+                            <td>
+                            <span>
+                                <a>
+                                    <i class="fas fa-eye"></i>
+                                </a>
+                                <a>
+                                    <i class="fas fa-edit"></i>
+                                </a>
+                                <a>
+                                    <i class="fas fa-trash-alt"></i>
+                                </a>
+                                {{ $file->fileName }}
+                            </span>
+                            </td>
+                            <td>
+                                <span>{{ $file->fileSize }} Bytes</span>
+                            </td>
+                            <td class="td-right">
+                                <span>{{ $file->fileExtension }}</span>
+                            </td>
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
+        </div>
 @endsection
