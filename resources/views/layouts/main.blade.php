69c9f2ee5aa96e52e6ac0ee2546e310ffd434acc
vardy <jarred.vardy@gmail.com>
2019-03-29 00:11:25 +0000

    Changed Reach button in nav bar to Reach Our COO

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -17,7 +17,7 @@
     <ul>
         <li><a href="{{ route('files') }}">Uploads</a></li>
         <li><a href="{{ route('quotations') }}">Quotations</a></li>
-        <li><a href="{{ route('reach') }}">Reach</a></li>
+        <li><a href="{{ route('reach') }}">Reach Our COO</a></li>
         <li><div class="vertical-separator"></div></li>
         <li>
             <form id="frm-logout" action="{{ route('logout') }}" method="POST" style="display: inline;">
