aaaabdbad9609bc7cbc1309c253b3c5e0d60135d
vardy <jarred.vardy@gmail.com>
2019-04-05 12:48:35 +0100

    Added link to request a quote via mailto with body-text formatting for email

--- resources/views/quotations/quote-nav-buttons.blade.php
+++ resources/views/quotations/quote-nav-buttons.blade.php
@@ -6,7 +6,7 @@
         </a>
     </span>
     <span id="request-button">
-        <a href="/quotations#">
+        <a href="mailto:connect@lexallo.com?subject=Client Quotation Request&amp;body=Please provide us with the following information and we’ll get back to you within 1 working day.%0A%0A[ Source Language ]%0A%0A%0A%0A[ Target Language ]%0A%0A%0A%0A[ Other Instructions / Comments ]%0A%0A%0A%0A[ Delivery Date and Time (please specify timezone) ]%0A%0A%0A%0A">
             <i class="fas fa-bolt quotes-nav-icon"></i><br>
             <span style="@if($current == 'request')text-decoration: underline; color: rgb(23, 53, 63);@endif">Request a quote</span>
         </a>
